6161405f408dfd6a388e362657adbe76ca894455
1611Aryan <agupta32_be19@thapar.edu>
2020-10-07 04:11:57 +0530

    responsive Home1

--- TS/main.js
+++ TS/main.js
@@ -1,20 +1,33 @@
 var toggle = $("#toggle");
 var seasame = $("#seasame");
 var header = $("header");
+var ignore = function () {
+    toggle.on("mouseenter", function () {
+        //alert("yo");
+        return false;
+    });
+};
+//menu closing when mouse leaves menu
 var vanish = function () {
     if (seasame.css("display") != "none") {
-        seasame.hide("swing");
-        //remove the X
-        toggle.toggleClass("fas fa-times ");
-        //insert the hamburger
-        toggle.toggleClass("fas fa-bars ");
+        if (toggle.mouseenter()) {
+            alert("hi");
+        }
+        else {
+            seasame.hide("swing");
+            //remove the X
+            toggle.toggleClass("fas fa-times ");
+            //insert the hamburger
+            toggle.toggleClass("fas fa-bars ");
+        }
     }
 };
-toggle.click(function () {
+//menu opening closing
+toggle.on("click touch", function () {
     seasame.toggle("swing");
     //remove the hamburger
     toggle.toggleClass("fas fa-bars ");
     //insert the X
     toggle.toggleClass("fas fa-times");
 });
-seasame.on("mouseleave", vanish);
+//seasame.on("mouseleave", vanish);
--- TS/main.ts
+++ TS/main.ts
@@ -1,18 +1,28 @@
 const toggle = $("#toggle");
 const seasame = $("#seasame");
 const header = $("header");
-
+const ignore = () => {
+  toggle.on("mouseenter", () => {
+    //alert("yo");
+    return false;
+  });
+};
+//menu closing when mouse leaves menu
 const vanish = () => {
   if (seasame.css("display") != "none") {
-    seasame.hide("swing");
-    //remove the X
-    toggle.toggleClass("fas fa-times ");
-    //insert the hamburger
-    toggle.toggleClass("fas fa-bars ");
+    if (toggle.mouseenter()) {
+      alert("hi");
+    } else {
+      seasame.hide("swing");
+      //remove the X
+      toggle.toggleClass("fas fa-times ");
+      //insert the hamburger
+      toggle.toggleClass("fas fa-bars ");
+    }
   }
 };
-
-toggle.click(() => {
+//menu opening closing
+toggle.on("click touch", function () {
   seasame.toggle("swing");
   //remove the hamburger
   toggle.toggleClass("fas fa-bars ");
@@ -20,4 +30,4 @@ toggle.click(() => {
   toggle.toggleClass("fas fa-times");
 });
 
-seasame.on("mouseleave", vanish);
+//seasame.on("mouseleave", vanish);
