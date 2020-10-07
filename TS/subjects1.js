7ccb00fa597140d9b2a8d9dfe3e2d189f08fa138
1611Aryan <agupta32_be19@thapar.edu>
2020-10-08 04:22:35 +0530

    1st year

--- TS/subjects1.js
+++ TS/subjects1.js
@@ -1,6 +1,10 @@
 var addSubject1 = function (subject) {
     var section = '<a href=""><section><span>' + subject + "</span></section></a>";
-    $("main").append(section);
+    $("#page1").append(section);
+};
+var addSubject1_2 = function (subject) {
+    var section = '<a href=""><section><span>' + subject + "</span></section></a>";
+    $("#page2").append(section);
 };
 var streams1 = [
     "Biotechnology",
@@ -20,7 +24,19 @@ var streams1 = [
     "Electrical and Computer ",
     "Communication MBA",
 ];
+//////*page1
 var noOfSubjects1 = streams1.length;
-for (var i = 0; i < noOfSubjects1; i++) {
+for (var i = 0; i < 8; i++) {
     addSubject1(streams1[i]);
 }
+////*page2
+for (var i = 8; i < noOfSubjects1; i++) {
+    addSubject1_2(streams1[i]);
+}
+////*pageChange
+$("#button").on("click touch", function () {
+    $("#page2").toggle();
+    $("#page1").toggle();
+    $("#button").toggleClass("fa-angle-left");
+    $("#button").toggleClass("fa-angle-right");
+});
--- TS/subjects1.ts
+++ TS/subjects1.ts
@@ -1,7 +1,12 @@
 const addSubject1 = (subject: string) => {
   const section =
     '<a href=""><section><span>' + subject + "</span></section></a>";
-  $("main").append(section);
+  $("#page1").append(section);
+};
+const addSubject1_2 = (subject: string) => {
+  const section =
+    '<a href=""><section><span>' + subject + "</span></section></a>";
+  $("#page2").append(section);
 };
 const streams1 = [
   "Biotechnology",
@@ -21,7 +26,20 @@ const streams1 = [
   "Electrical and Computer ",
   "Communication MBA",
 ];
+//////*page1
 const noOfSubjects1 = streams1.length;
-for (let i = 0; i < noOfSubjects1; i++) {
+for (let i = 0; i < 8; i++) {
   addSubject1(streams1[i]);
 }
+////*page2
+for (let i = 8; i < noOfSubjects1; i++) {
+  addSubject1_2(streams1[i]);
+}
+
+////*pageChange
+$("#button").on("click touch", () => {
+  $("#page2").toggle();
+  $("#page1").toggle();
+  $("#button").toggleClass("fa-angle-left");
+  $("#button").toggleClass("fa-angle-right");
+});
