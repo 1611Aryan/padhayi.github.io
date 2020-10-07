774e2e43e6bf83293f7bad02551930376a461e9c
1611Aryan <agupta32_be19@thapar.edu>
2020-10-08 05:06:18 +0530

    2ndyear 3rdyear done

--- TS/subjects2.js
+++ TS/subjects2.js
@@ -1,9 +1,42 @@
 var addSubject2 = function (subject) {
     var section = '<a href=""><section><span>' + subject + "</span></section></a>";
-    $("main").append(section);
+    $("#page1").append(section);
 };
-var streams2 = ["Computer Engineering", "Electronics"];
-var noOfSubjects2 = 12;
-for (var i = 0; i < noOfSubjects2; i++) {
-    addSubject2("stream");
+var addSubject2_2 = function (subject) {
+    var section = '<a href=""><section><span>' + subject + "</span></section></a>";
+    $("#page2").append(section);
+};
+var streams2 = [
+    "Biotechnology",
+    "Chemical",
+    "Civil",
+    "Computer Engineering",
+    "Computer Science & Engineering",
+    "Computer Science and Business",
+    "Electronics and Communication",
+    "Electrical",
+    "Electronics and Instrumentation ",
+    "Mechanical",
+    "Mechatronics",
+    "Mechanical Production",
+    "Electronics and Computer",
+];
+//////*page1
+var noOfSubjects2 = streams2.length;
+for (var i = 0; i < 8; i++) {
+    addSubject2(streams2[i]);
 }
+////*page2
+for (var i = 8; i < noOfSubjects2; i++) {
+    addSubject2_2(streams2[i]);
+}
+////*pageChange
+$("#button").on("click touch", function () {
+    $("#page2").toggle("slow");
+    $("#page1").toggle("slow");
+    $("#button").toggleClass("fa-angle-left");
+    $("#button").toggleClass("fa-angle-right");
+});
+$("#toggle").on("click touch", function () {
+    $("#buttons").toggle("swing");
+});
--- TS/subjects3.js
+++ TS/subjects3.js
@@ -1,9 +1,42 @@
 var addSubject3 = function (subject) {
     var section = '<a href=""><section><span>' + subject + "</span></section></a>";
-    $("main").append(section);
+    $("#page1").append(section);
 };
-var streams3 = ["Computer Engineering", "Electronics"];
-var noOfSubjects3 = 12;
-for (var i = 0; i < noOfSubjects3; i++) {
-    addSubject3("stream");
+var addSubject3_2 = function (subject) {
+    var section = '<a href=""><section><span>' + subject + "</span></section></a>";
+    $("#page2").append(section);
+};
+var streams3 = [
+    "Biotechnology",
+    "Chemical",
+    "Civil",
+    "Computer Engineering",
+    "Computer Science & Engineering",
+    "Computer Science and Business",
+    "Electronics and Communication",
+    "Electrical",
+    "Electronics and Instrumentation ",
+    "Mechanical",
+    "Mechatronics",
+    "Mechanical Production",
+    "Electronics and Computer",
+];
+//////*page1
+var noOfSubjects3 = streams3.length;
+for (var i = 0; i < 8; i++) {
+    addSubject3(streams3[i]);
 }
+////*page2
+for (var i = 8; i < noOfSubjects3; i++) {
+    addSubject3_2(streams3[i]);
+}
+////*pageChange
+$("#button").on("click touch", function () {
+    $("#page2").toggle("slow");
+    $("#page1").toggle("slow");
+    $("#button").toggleClass("fa-angle-left");
+    $("#button").toggleClass("fa-angle-right");
+});
+$("#toggle").on("click touch", function () {
+    $("#buttons").toggle("swing");
+});
