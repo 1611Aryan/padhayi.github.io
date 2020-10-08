5078923c3d25eea3f36f928099ff43d8fcb38c46
1611Aryan <agupta32_be19@thapar.edu>
2020-10-09 00:11:08 +0530

    dark mode normal

--- TS/darkmode.js
+++ TS/darkmode.js
@@ -1,15 +1,58 @@
 var darkmode = '<link rel="stylesheet" type="text/css" href="SASS/darkmode.css">';
+///////////////
 var darkmode2 = '<link rel="stylesheet" type="text/css" href="../SASS/darkmode.css">';
-var i = 0;
+///////////////////////////
+var setTheme = function (color) {
+    sessionStorage.setItem("scheme", color);
+};
+var setCounter = function (j) {
+    sessionStorage.setItem("memory", j);
+};
+var getTheme = function () {
+    return sessionStorage.getItem("scheme");
+};
+var getCounter = function () {
+    return sessionStorage.getItem("memory");
+};
+///////////////////////////
+if (typeof getCounter() != "string") {
+    setCounter("0");
+}
+///////////////////////
+var i = parseInt(sessionStorage.getItem("memory"));
+///////////////////
+var darkCSS = function () {
+    if (getTheme() == "dark") {
+        $("header").append(darkmode);
+        $("header").append(darkmode2);
+    }
+    if (getTheme() != "dark" || getTheme() == "light") {
+        $(darkmode).prop("disabled", true);
+        $(darkmode2).prop("disabled", true);
+        if (i % 2 != 0) {
+            window.location.reload();
+            i++;
+        }
+    }
+};
+///////////////
 var enable = function (i) {
     if (i % 2 == 0) {
-        $("head").append(darkmode);
-        $("head").append(darkmode2);
+        setTheme("dark");
+        darkCSS();
     }
-    else
-        return false;
+    else {
+        setTheme("light");
+        darkCSS();
+    }
 };
+////////////////////////
 $("#darkModeToggle").on("click touch", function () {
     enable(i);
+    console.log(i);
+    setCounter(i + "");
     i++;
+    console.log(sessionStorage.getItem("memory"));
 });
+////////////////////////
+setInterval(darkCSS, 100);
--- TS/darkmode.ts
+++ TS/darkmode.ts
@@ -1,15 +1,65 @@
 const darkmode: string =
   '<link rel="stylesheet" type="text/css" href="SASS/darkmode.css">';
+///////////////
 const darkmode2 =
   '<link rel="stylesheet" type="text/css" href="../SASS/darkmode.css">';
-let i = 0;
+///////////////////////////
+const setTheme = (color: string) => {
+  sessionStorage.setItem("scheme", color);
+};
+const setCounter = (j: string) => {
+  sessionStorage.setItem("memory", j);
+};
+const getTheme = () => {
+  return sessionStorage.getItem("scheme");
+};
+const getCounter = () => {
+  return sessionStorage.getItem("memory");
+};
+///////////////////////////
+if (typeof getCounter() != "string") {
+  setCounter("0");
+}
+///////////////////////
+
+let i: number = parseInt(sessionStorage.getItem("memory"));
+
+///////////////////
+const darkCSS = () => {
+  if (getTheme() == "dark") {
+    $("header").append(darkmode);
+    $("header").append(darkmode2);
+  }
+  if (getTheme() != "dark" || getTheme() == "light") {
+    $(darkmode).prop("disabled", true);
+    $(darkmode2).prop("disabled", true);
+    if (i % 2 != 0) {
+      window.location.reload();
+      i++;
+    }
+  }
+};
+///////////////
 const enable = (i: number) => {
   if (i % 2 == 0) {
-    $("head").append(darkmode);
-    $("head").append(darkmode2);
-  } else return false;
+    setTheme("dark");
+    darkCSS();
+  } else {
+    setTheme("light");
+    darkCSS();
+  }
 };
+
+////////////////////////
 $("#darkModeToggle").on("click touch", () => {
   enable(i);
+  console.log(i);
+  setCounter(i + "");
   i++;
+
+  console.log(sessionStorage.getItem("memory"));
 });
+
+////////////////////////
+
+setInterval(darkCSS, 100);
